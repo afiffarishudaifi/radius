f3b49dcfe0606965f471cc1a3eb4f5a3a10455d9
afiffarishudaifi <afiffaris5@gmail.com>
2020-10-30 14:27:36 +0700

    pertama

---
+++ _partials/navbar.php
@@ -0,0 +1,27 @@
+<nav class="main-header navbar navbar-expand navbar-danger navbar-dark border-bottom-0">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="index3.html" class="nav-link">Home</a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <!-- Messages Dropdown Menu -->
+        <li class="nav-item">
+            <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">Afif Faris Hudaifi
+                <i class="fas fa-user fa-fw"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item " data-toggle="modal" data-target="#logoutModal" href=""><i
+                        class="fas fa-power-off mr-1"></i>Keluar</a>
+            </div>
+        </li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include('./_partials/head.php'); ?>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <?php include('./_partials/navbar.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php include('./_partials/sidebar.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Main content -->
+            <section class="content">
+                <!-- container-fluid -->
+                <div class="container-fluid">
+
+                </div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <?php include('./_partials/footer.php'); ?>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./_partials/wrapper -->
+
+    <?php include('./_partials/js.php'); ?>
+</body>
+
+</html>
